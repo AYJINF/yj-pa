6b257750008d9c8835d984f9626ef25c6279c768
tracer-ics2022 <tracer@njuics.org>
2022-09-26 10:02:44 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:02:44 up 44 min,  1 user,  load average: 0.08, 0.15, 0.11

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -81,7 +81,7 @@ static int cmd_x(char *args){
   int x_N, x_EXPR;
   sscanf(args, "%d %x", &x_N, &x_EXPR);
   for(int i = 0; i < x_N; i++){
-    printf("%x: %08x", x_EXPR, paddr_read(x_EXPR, 4));
+    printf("%x: %08x\n", x_EXPR, paddr_read(x_EXPR, 4));
     x_EXPR += 4;
   }
   return 0;
