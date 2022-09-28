1f4586463a4630a5b55b1bac54957faab98dbd02
tracer-ics2022 <tracer@njuics.org>
2022-09-28 20:40:52 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:40:52 up  9:32,  1 user,  load average: 0.31, 0.33, 0.28

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -48,8 +48,8 @@ static struct rule {
   {"\\/", '/'},                 // divide 
   {"\\(", '('},                 // left parenthesis
   {"\\)", ')'},                 // right parenthesis
-  {"[0-9]+", TK_DIGIT},         // decimal integer
   {"0x[0-9A-Fa-f]+", TK_HEX},   // hexadecimal-number
+  {"[0-9]+", TK_DIGIT},         // decimal integer
   {"\\$[$a-z0-9]+", TK_REG},    //reg_name
 
 };
