252ce8b4fc117e3dec610abf50af369e9dbb9ed0
tracer-ics2022 <tracer@njuics.org>
2022-09-28 16:22:11 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:22:11 up  5:13,  1 user,  load average: 0.27, 0.27, 0.32

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -33,10 +33,7 @@ word_t isa_reg_str2val(const char *s, bool *success) {
   for(int i = 0; i < 32; i++){
     if(!strcmp(s, regs[i])){
       *success = true;
-      int t = 0;
-      printf("???%x", gpr(i));
-      // sscanf(gpr(i), "%x", &t);
-      return t;
+      return gpr(i);
     }
   }
   assert(0);
