afc76ccb021a9ddccc2ec1c12dd9044f9dfdcfd7
tracer-ics2022 <tracer@njuics.org>
2022-09-28 21:51:23 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:51:23 up 10:42,  1 user,  load average: 0.39, 0.29, 0.23

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -242,7 +242,7 @@ word_t expr_eval(int p, int q){
       }
     }
     if(tokens[op].type == TK_DEREF){
-      return paddr_read(expr_eval(op+1, q), 1);
+      return paddr_read(expr_eval(op+1, q), 4);
     }
     if(tokens[op].type == TK_MIDI){
       return -expr_eval(op+1, q);
