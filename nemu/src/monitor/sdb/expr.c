cb80a8ec0662986709a3af607078d7ae3f1b123b
tracer-ics2022 <tracer@njuics.org>
2022-09-28 21:47:50 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:47:50 up 10:39,  1 user,  load average: 0.17, 0.25, 0.22

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -210,16 +210,16 @@ word_t expr_eval(int p, int q){
     assert(0);
   }
   else if(p == q){
-    //word_t tmp = 0;
+    word_t tmp = 0;
     bool success = true;
     switch (tokens[p].type)
     {
     case TK_DIGIT:
       return atoi(tokens[p].str);
     case TK_HEX:
-      int t = 0;
-      sscanf(tokens[p].str, "%x", &t);
-      return t;
+      sscanf(tokens[p].str, "%x", &tmp);
+      printf("tmp=%d\n", tmp);
+      return tmp;
     case TK_REG:
       return isa_reg_str2val(tokens[p].str+1, &success);
     default:
