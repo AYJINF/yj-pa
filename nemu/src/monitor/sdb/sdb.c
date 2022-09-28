124bf2afb7b0318465b5388e3311792a55814912
tracer-ics2022 <tracer@njuics.org>
2022-09-28 15:30:19 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:30:19 up  4:21,  1 user,  load average: 0.85, 0.56, 0.41

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -91,7 +91,7 @@ static int cmd_x(char *args){
 static int cmd_p(char *args){
   bool success = true;
   if(success)printf("%d", expr(args, &success));
-  assert(0);
+  else assert(0);
   return 0;
 }
 
