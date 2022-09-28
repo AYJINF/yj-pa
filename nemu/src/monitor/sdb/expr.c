309f4d8c640a326f96104c8ef834246db279def6
tracer-ics2022 <tracer@njuics.org>
2022-09-28 19:34:00 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:34:00 up  8:25,  1 user,  load average: 0.52, 0.29, 0.22

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -203,6 +203,7 @@ int s_pri(char s){
 
 
 word_t expr_eval(int p, int q){
+  printf("p=%d, q=%d\n", p, q);
   if(p > q){
     printf("A bad expression!");
     assert(0);
@@ -238,7 +239,6 @@ word_t expr_eval(int p, int q){
         int s2 = s_pri(tokens[i].type);
         if(s1 <= s2)op = i;
       }
-      printf("op=%d", op);
     }
     int val1 = expr_eval(p, op - 1);
     int val2 = expr_eval(op + 1, q);
