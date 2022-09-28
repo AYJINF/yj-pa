cc4164d67bb9a2cfbf9b2b48cddd22f60433cf73
tracer-ics2022 <tracer@njuics.org>
2022-09-28 19:53:30 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:53:30 up  8:44,  1 user,  load average: 0.42, 0.39, 0.38

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -238,6 +238,7 @@ word_t expr_eval(int p, int q){
         int s1 = s_pri(tokens[op].type);
         int s2 = s_pri(tokens[i].type);
         if(s1 <= s2)op = i;
+        printf("op_type=%d\n", tokens[i].type);
       }
     }
     printf("op=%d\n", op);
