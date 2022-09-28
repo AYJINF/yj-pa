07851eeeba1005d29827c2b82d9a030676fceb7f
tracer-ics2022 <tracer@njuics.org>
2022-09-28 23:05:39 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:05:39 up 11:56,  1 user,  load average: 0.26, 0.29, 0.27

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -97,6 +97,7 @@ static bool make_token(char *e) {
       if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
         char *substr_start  = e + position;
         int substr_len = pmatch.rm_eo;
+        printf("len=%d\n", substr_len);
 
         Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
             i, rules[i].regex, position, substr_len, substr_len, substr_start);
