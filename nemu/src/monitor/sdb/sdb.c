920fb10b46d613f2eb19b275bee59741750f1da3
tracer-ics2022 <tracer@njuics.org>
2022-09-29 14:34:41 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:34:41 up 23 min,  1 user,  load average: 0.14, 0.17, 0.13

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -105,7 +105,7 @@ static struct {
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
   { "si", "'si N'.Step through N instructions and then pause execution. N defaults to 1 if not given", cmd_si },
-  { "info", "Print state of the program\\'info r' for state of the register\\'info w' for information of the watchpoint", cmd_info },
+  { "info", "Print state of the program\n'info r' for state of the register\n'info w' for information of the watchpoint", cmd_info },
   { "x", "'x N EXPR'. Evaluate EXPR, use the result as the starting memory address then output consecutive N 4-bytes in hexadecimal", cmd_x },
   { "p", "'p EXPR'. Evaluate EXPR", cmd_p}
 
