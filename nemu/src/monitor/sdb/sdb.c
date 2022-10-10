78417e2bce4d75c27dc59c68d2cce040bd18a114
tracer-ics2022 <tracer@njuics.org>
2022-10-11 00:31:02 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:31:02 up  8:34,  1 user,  load average: 0.44, 0.36, 0.30

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -108,9 +108,10 @@ static int cmd_w(char *args){
 }
 
 static int cmd_d(char *args){
-  printf("cmd_d %d\n", atoi(args));
+  
   if(args != NULL){
     delete_wp(atoi(args));
+    printf("cmd_d %d\n", atoi(args));
   }
   else assert(0);
   return 0;
