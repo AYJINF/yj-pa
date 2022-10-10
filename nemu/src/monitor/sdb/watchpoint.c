4ebb029c766d9c9f788d39450c23d5fed33323b2
tracer-ics2022 <tracer@njuics.org>
2022-10-11 01:19:34 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:19:34 up  9:22,  1 user,  load average: 0.28, 0.32, 0.35

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -55,8 +55,8 @@ void delete_wp(int NO){
   WP *cur = head;
   while(cur != NULL){
     if(cur->NO == NO){
-      pre->next = cur->next;
-      printf("www  %d\n", cur->NO);
+      if(pre != NULL)pre->next = cur->next;
+      else pre = cur->next;
       free_wp(cur);
       return;
     }
