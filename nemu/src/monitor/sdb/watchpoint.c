da4c173dceceebb7e648ee6675a7b1a15308cc50
tracer-ics2022 <tracer@njuics.org>
2022-10-11 01:48:00 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:48:00 up  9:51,  1 user,  load average: 0.27, 0.31, 0.28

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -33,7 +33,7 @@ void init_wp_pool() {
 
 /* Display states of the watchpoints in use */
 void isa_wp_display(){
-  WP *cur = &(*head);
+  WP *cur = head;
   if(cur == NULL)printf("No watchpoints\n");
   while(cur != NULL){
     printf("NO.%d,\t %s = %d\n", cur->NO, cur->var, cur->data);
