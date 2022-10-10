4bfe304cb30fcbf329ca3db2dcce7c30be766e80
tracer-ics2022 <tracer@njuics.org>
2022-10-10 19:18:42 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:18:42 up  3:22,  1 user,  load average: 0.27, 0.27, 0.27

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -28,9 +28,9 @@ static WP wp_pool[NR_WP] = {};
 static WP *head = NULL, *free_ = NULL;
 
 void init_wp_pool() {
-  printf("???");
   int i;
   for (i = 0; i < NR_WP; i ++) {
+    printf("%d\n", i);
     wp_pool[i].NO = i;
     wp_pool[i].next = (i == NR_WP - 1 ? NULL : &wp_pool[i + 1]);
   }
