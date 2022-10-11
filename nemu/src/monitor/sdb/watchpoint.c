55a76ad4cb2b29e32b3390752fb2cefb3f064453
tracer-ics2022 <tracer@njuics.org>
2022-10-11 16:18:15 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:18:15 up  5:38,  1 user,  load average: 0.12, 0.11, 0.14

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -86,7 +86,7 @@ void scan_all_wps(){
     word_t tmp = expr(cur->var, &success);
     if(!success) assert(0);
     if(tmp != cur->data){
-      printf("The value of watchpoint NO.%d has changed from %d to %d.\n", cur->NO, cur->data, tmp);
+      printf("The value of watchpoint NO.%d has changed.\n Old value = %d\n  New value = %d.\n", cur->NO, cur->data, tmp);
       nemu_state.state = NEMU_STOP;
       cur->data = tmp;
     }
