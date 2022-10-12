53c936a3729e3241fd312e611de36ffda2050c6a
tracer-ics2022 <tracer@njuics.org>
2022-10-13 01:30:25 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:30:25 up 14:12,  1 user,  load average: 0.44, 0.39, 0.46

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -165,22 +165,22 @@ void sdb_mainloop() {
 
 
   /* test for EXPRs (the amount of data is not general) */
-  FILE *fp = fopen("../../../tools/gen-expr/input", "r");
-  unsigned ans[6622];
-  char test_expr[6622][1000];
-  // while(!feof(fp))
-  for(int i = 0; i < 6622; i++){
-    printf("yyy\n");
-    printf("i=%d", i);
-    int w1 = fscanf(fp, "%u", &ans[i]);
-    if(w1)printf("line%d, w1=%d", i+1, w1);
-    char *w2 = fgets(test_expr[i], 1000, fp);
-    if(w2 == NULL)printf("line%d, w2==NULL", i+1);
-    bool if_success = true;
-    word_t tmp = expr(test_expr[i], &if_success);
-    if(if_success == false) printf("line %d fail to be cal\n", i+1);
-    if(tmp != ans[i]) printf("line%d is wrong. %u != %u", i+1, tmp, ans[i]);
-  }
+  // FILE *fp = fopen("../../../tools/gen-expr/input", "r");
+  // unsigned ans[6622];
+  // char test_expr[6622][1000];
+  // // while(!feof(fp))
+  // for(int i = 0; i < 6622; i++){
+  //   printf("yyy\n");
+  //   printf("i=%d", i);
+  //   int w1 = fscanf(fp, "%u", &ans[i]);
+  //   if(w1)printf("line%d, w1=%d", i+1, w1);
+  //   char *w2 = fgets(test_expr[i], 1000, fp);
+  //   if(w2 == NULL)printf("line%d, w2==NULL", i+1);
+  //   bool if_success = true;
+  //   word_t tmp = expr(test_expr[i], &if_success);
+  //   if(if_success == false) printf("line %d fail to be cal\n", i+1);
+  //   if(tmp != ans[i]) printf("line%d is wrong. %u != %u", i+1, tmp, ans[i]);
+  // }
 
   if (is_batch_mode) {
     cmd_c(NULL);
