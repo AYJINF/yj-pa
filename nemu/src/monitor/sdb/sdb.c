8195328fb97dde6e8f594d71fd61e98501076c1d
tracer-ics2022 <tracer@njuics.org>
2022-10-13 01:39:52 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:39:52 up 14:21,  1 user,  load average: 0.49, 0.61, 0.56

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -171,9 +171,7 @@ void sdb_mainloop() {
   // while(!feof(fp))
   for(int i = 0; i < 6622; i++){
     printf("i=%d\n", i);
-    int w1 = fscanf(fp, "%u", &ans[i]);
-    printf("yyy");
-    if(w1)printf("line%d, w1=%d", i+1, w1);
+    if(fscanf(fp, "%u", &ans[i]))printf("line%d has problem", i+1);
     char *w2 = fgets(test_expr[i], 1000, fp);
     if(w2 == NULL)printf("line%d, w2==NULL", i+1);
     bool if_success = true;
