efc500400e9e6a00bd76a5f97e5867c241209d7f
tracer-ics2022 <tracer@njuics.org>
2022-10-13 17:07:32 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:07:32 up  1:46,  1 user,  load average: 0.24, 0.35, 0.38

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -54,7 +54,8 @@ int main(int argc, char *argv[]) {
     test_expr[strlen(test_expr)]='\0';
     printf("%d %s\n",a,test_expr);
     bool check;
-    printf("result=%u\n", expr(test_expr,&check));
+    int t = expr(test_expr,&check);
+    printf("result=%u\n", t);
     if(a!=expr(test_expr,&check)){
       printf("shit\n");
     }
