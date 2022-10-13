6f6413fc0ad960fb59022a67f3d8ae7dfce94e87
tracer-ics2022 <tracer@njuics.org>
2022-10-13 17:46:43 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:46:43 up  2:26,  1 user,  load average: 0.26, 0.43, 0.43

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -38,16 +38,16 @@ int main(int argc, char *argv[]) {
   /* Test for EXPRs (Make engine_start() noted if use it) */
   FILE *fp = fopen("tools/gen-expr/input", "r");
   unsigned a;
-  char test_expr[1000];
-  int flag[6622] = {0}; // to see if there are wrong answers
-  unsigned ans[6622] = {0};
-  unsigned r[6622] = {0};
+  char test_expr[1000]="0xfe43";
+  // int flag[6622] = {0}; // to see if there are wrong answers
+  // unsigned ans[6622] = {0};
+  // unsigned r[6622] = {0};
   if(fp==NULL){
     printf("The fp is error\n");
   }
-  int j = 0;
-  for(int i = 0; i < 6622; i++){
-    memset(test_expr,0,sizeof(test_expr));
+  // int j = 0;
+  for(int i = 0; i < 1; i++){
+    // memset(test_expr,0,sizeof(test_expr));
     int w1=fscanf(fp, "%u", &a);
     char *w2=fgets(test_expr, 1000, fp);
     if(w1&&w2){
@@ -58,16 +58,17 @@ int main(int argc, char *argv[]) {
     bool check;
     word_t t = expr(test_expr,&check);
     if(a!=t){
-      flag[j] = i+1;
-      ans[j] = a;
-      r[j] = t;
-      j++;
+      // flag[j] = i+1;
+      // ans[j] = a;
+      // r[j] = t;
+      // j++;
+      printf("uyyyy\n");
     }
   }
   // printf("flag=%d", flag);
-  for(int k = 0; flag[k] != 0; k++){
-    printf("line=%d, ans=%u, result=%u\n", flag[k], ans[k], r[k]);
-  }
+  // for(int k = 0; flag[k] != 0; k++){
+  //   printf("line=%d, ans=%u, result=%u\n", flag[k], ans[k], r[k]);
+  // }
   nemu_state.state = NEMU_QUIT;
 
   return is_exit_status_bad();
