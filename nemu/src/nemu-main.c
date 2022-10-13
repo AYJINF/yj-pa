ea7dbd1e77e91c637ba7094d61c0f1db16af4c11
tracer-ics2022 <tracer@njuics.org>
2022-10-13 16:47:25 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:47:25 up  1:26,  1 user,  load average: 0.26, 0.34, 0.42

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -33,29 +33,29 @@ int main(int argc, char *argv[]) {
 #endif
 
   /* Start engine. */
-  // engine_start();
+  engine_start();
   
-  FILE *fp = fopen("tools/gen-expr/input", "r");
-  unsigned a;
-  char test_expr[1000];
-  if(fp==NULL){
-    printf("The fp is error\n");
-  }
-  for(int i = 0; i < 6622; i++){
-    memset(test_expr,0,sizeof(test_expr));
-    int w1=fscanf(fp, "%u", &a);
-    char *w2=fgets(test_expr, 1000, fp);
-    if(w1&&w2){
-      w1=0;
-    }
-    printf("%d %s\n",a,test_expr);
-    test_expr[strlen(test_expr)-1]='\0';
-    bool check;
-    if(a!=expr(test_expr,&check)){
-      printf("shit\n");
-    }
+  // FILE *fp = fopen("tools/gen-expr/input", "r");
+  // unsigned a;
+  // char test_expr[1000];
+  // if(fp==NULL){
+  //   printf("The fp is error\n");
+  // }
+  // for(int i = 0; i < 6622; i++){
+  //   memset(test_expr,0,sizeof(test_expr));
+  //   int w1=fscanf(fp, "%u", &a);
+  //   char *w2=fgets(test_expr, 1000, fp);
+  //   if(w1&&w2){
+  //     w1=0;
+  //   }
+  //   printf("%d %s\n",a,test_expr);
+  //   test_expr[strlen(test_expr)-1]='\0';
+  //   bool check;
+  //   if(a!=expr(test_expr,&check)){
+  //     printf("shit\n");
+  //   }
 
-  }
+  // }
 
   return is_exit_status_bad();
 }
