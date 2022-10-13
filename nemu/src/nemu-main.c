24a22301e660c474e1bc12a5f5f2076cf846e970
tracer-ics2022 <tracer@njuics.org>
2022-10-13 15:36:07 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:36:07 up 15 min,  1 user,  load average: 0.30, 0.26, 0.20

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -35,10 +35,10 @@ int main(int argc, char *argv[]) {
    /* test for EXPRs (the amount of data is not general) */
   FILE *fp = fopen("../../../tools/gen-expr/input", "r");
   // while(!feof(fp))
+  int ans = 0;
+  char test_expr[1000] = {"0"};
   for(int i = 0; i < 6622; i++){
     printf("i=%d\n", i);
-    int ans = 0;
-    char test_expr[1000] = {"0"};
     int w1 = fscanf(fp, "%u", &ans);
     if(w1)printf("line%d, w1=%d", i+1, w1);
     char *w2 = fgets(test_expr, 1000, fp);
@@ -47,6 +47,8 @@ int main(int argc, char *argv[]) {
     word_t tmp = expr(test_expr, &if_success);
     if(if_success == false) printf("line %d fail to be cal\n", i+1);
     if(tmp != ans) printf("line%d is wrong. %u != %u", i+1, tmp, ans);
+    ans = 0;
+    memset(test_expr, 0, sizeof(test_expr));
   }
 
   return is_exit_status_bad();
