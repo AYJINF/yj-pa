d228551c7e32571b2edb4678a2044a37bf71aef4
tracer-ics2022 <tracer@njuics.org>
2022-10-13 16:00:13 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:00:13 up 39 min,  1 user,  load average: 0.78, 0.55, 0.48

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -43,7 +43,7 @@ int main(int argc, char *argv[]) {
   for(int i = 0; i < 6622; i++){
     printf("i=%d\n", i);
     int w1 = fscanf(fp, "%d", &ans);
-    printf("yyy\n");
+    printf("yyy%d\n",ans);
     if(w1)printf("line%d, w1=%d", i+1, w1);
     // char *w2 = fgets(test_expr, 1000, fp);
     // if(w2 == NULL)printf("line%d, w2==NULL", i+1);
