634457e207d5ba3ff0fcdb1b0bdb0d6aa983e008
tracer-ics2022 <tracer@njuics.org>
2022-10-13 16:26:22 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:26:22 up  1:05,  1 user,  load average: 0.59, 0.63, 0.53

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -271,6 +271,7 @@ word_t expr(char *e, bool *success) {
     *success = false;
     return 0;
   }
+  printf("ss\n");
   // to find out the dereference
   for (int i = 0; i < nr_token; i ++) {
   if (tokens[i].type == '*' && 
