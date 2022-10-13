268bd85ea09e9930386a35458969d6c121be69df
tracer-ics2022 <tracer@njuics.org>
2022-10-13 15:43:33 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:43:33 up 22 min,  1 user,  load average: 1.05, 0.50, 0.32

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -33,7 +33,7 @@ int main(int argc, char *argv[]) {
   /* Start engine. */
   // engine_start();
    /* test for EXPRs (the amount of data is not general) */
-  FILE *fp = fopen("../tools/gen-expr/input", "r");
+  FILE *fp = fopen("tools/gen-expr/input", "r");
   // while(!feof(fp))
   int ans = 0;
   if(fp==NULL)printf("shit\n");
