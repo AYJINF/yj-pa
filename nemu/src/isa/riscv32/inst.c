d41c08d3766dcf61863cb770cee1ea8966bfd141
tracer-ics2022 <tracer@njuics.org>
2022-10-28 20:14:49 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:14:49 up 10:34,  1 user,  load average: 0.35, 0.40, 0.29

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -32,7 +32,7 @@ enum {
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
-#define immR() do { *imm = 0; } while(0)
+#define immR() do { *imm = SEXT(BITS(i, 31, 25), 7); } while(0)
 #define immJ() do { *imm = (((SEXT(BITS(i, 31, 31), 1) << 8 | BITS(i, 19, 12)) << 1 | BITS(i, 20, 20)) << 10 | BITS(i, 30, 21)) << 1; } while(0);
 #define immB() do { *imm = (((SEXT(BITS(i, 31, 31), 1) << 1 | BITS(i, 7, 7)) << 6 | BITS(i, 30, 25)) << 4 | BITS(i, 11, 8)) << 1; } while(0)
 
@@ -75,6 +75,7 @@ static int decode_exec(Decode *s) {
 
   INSTPAT("??????? ????? ????? 000 ????? 11000 11", beq    , B, if(src1 == src2) s->dnpc = s->pc + imm); // for add
   INSTPAT("0000000 ????? ????? 000 ????? 01100 11", add    , R, R(dest) = src1 + src2); 
+  INSTPAT("0100000 ????? ????? 000 ????? 01100 11", sub    , R, R(dest) = src1 - src2); 
 
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
