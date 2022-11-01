0004ea12a7bda1878ce7e92d955d6874e574aca1
tracer-ics2022 <tracer@njuics.org>
2022-11-01 10:05:56 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:05:56 up 42 min,  1 user,  load average: 1.07, 0.72, 0.57

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -42,40 +42,40 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
     // int64_t num_t; // to be modified for more formats
     // int i = 0;
     // int count = 0;
-    // switch (*fmt)
-    // {
-    //   case '%':
-    //     *dst++ = '%';
-    //     break;
-    //   case 's':
-    //     s = va_arg(ap, char*);
-    //     s_len = strlen(s);
-    //     for(i = 0; i < field_width - s_len; i++)
-    //       *dst++ = flag_0 ? '0' : ' ';
-    //     while(s_len--)
-    //       *dst++ = *s++;
-    //     break;
-    //   case 'd':
-    //     num_t = va_arg(ap, int);
-    //     if(num_t == 0)
-    //       num_tmp[count++] = '0';
-    //     if(num_t < 0){
-    //       num_t *= -1;
-    //       *dst++ = '-';
-    //     }
-    //     while(num_t){
-    //       num_tmp[count++] = '0' + (num_t % 10);
-    //       num_t /= 10;
-    //     }
-    //     for(i = 0; i < field_width - count; i++)
-    //       *dst++ = flag_0 ? '0' : ' ';
-    //     while(count--){
-    //       *dst++ = num_tmp[count];
-    //     }
-    //     break;
-    //   default:
-    //     break;
-    // }
+    switch (*fmt)
+    {
+      case '%':
+        *dst++ = '%';
+        break;
+      case 's':
+        // s = va_arg(ap, char*);
+        // s_len = strlen(s);
+        // for(i = 0; i < field_width - s_len; i++)
+        //   *dst++ = flag_0 ? '0' : ' ';
+        // while(s_len--)
+        //   *dst++ = *s++;
+        break;
+      case 'd':
+        // num_t = va_arg(ap, int);
+        // if(num_t == 0)
+        //   num_tmp[count++] = '0';
+        // if(num_t < 0){
+        //   num_t *= -1;
+        //   *dst++ = '-';
+        // }
+        // while(num_t){
+        //   num_tmp[count++] = '0' + (num_t % 10);
+        //   num_t /= 10;
+        // }
+        // for(i = 0; i < field_width - count; i++)
+        //   *dst++ = flag_0 ? '0' : ' ';
+        // while(count--){
+        //   *dst++ = num_tmp[count];
+        // }
+        break;
+      default:
+        break;
+    }
     fmt++;
   }
   *dst = '\0';
