d1e0a238e25682536b7646677d581ee58498f8d9
tracer-ics2022 <tracer@njuics.org>
2022-11-20 14:48:04 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:48:04 up  3:55,  1 user,  load average: 0.30, 0.37, 0.35

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -30,19 +30,19 @@ CPU_state cpu = {};
 uint64_t g_nr_guest_inst = 0;
 static uint64_t g_timer = 0; // unit: us
 static bool g_print_step = false;
-// #ifdef CONFIG_IRINGBUF
-//   static int pos = 0;
-//   static char iringbuf[16][128];
-// #endif
+#ifdef CONFIG_IRINGBUF
+  static int pos = 0;
+  static char iringbuf[16][128];
+#endif
 
 void device_update();
 
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
-// #ifdef CONFIG_IRINGBUF
-//   pos%=16;
-//   strcpy(iringbuf[pos], _this->logbuf);
-//   pos++;
-// #endif
+#ifdef CONFIG_IRINGBUF
+  pos%=16;
+  strcpy(iringbuf[pos], _this->logbuf);
+  pos++;
+#endif
 #ifdef CONFIG_ITRACE_COND
   if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
 #endif
@@ -101,15 +101,15 @@ static void statistic() {
 }
 
 void assert_fail_msg() {
-  // #ifdef CONFIG_IRINGBUF
-  //   printf("\033[0;31mThere are error instructions!\033[0m\n");
-  //   for(int i = 0; i < 16; i++){
-  //     if(i == (pos-1)%16)
-  //       printf("\033[0;31m-->%s\033[0m\n", iringbuf[i]);
-  //     else
-  //       printf("\033[0;33m   %s\033[0m\n", iringbuf[i]);
-  //   }
-  // #endif
+  #ifdef CONFIG_IRINGBUF
+    printf("\033[0;31mThere are error instructions!\033[0m\n");
+    for(int i = 0; i < 16; i++){
+      if(i == (pos-1)%16)
+        printf("\033[0;31m-->%s\033[0m\n", iringbuf[i]);
+      else
+        printf("\033[0;33m   %s\033[0m\n", iringbuf[i]);
+    }
+  #endif
   isa_reg_display();
   statistic();
 }
