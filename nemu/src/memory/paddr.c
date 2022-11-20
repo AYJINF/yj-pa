4d893c377412c9a5f8acf88ecd9f40848b91312b
tracer-ics2022 <tracer@njuics.org>
2022-11-20 11:23:32 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:23:32 up 31 min,  1 user,  load average: 0.85, 0.66, 0.48

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -68,7 +68,7 @@ word_t paddr_read(paddr_t addr, int len) {
 
 void paddr_write(paddr_t addr, int len, word_t data) {
   #ifdef CONFIG_MTRACE
-    printf("Write Memory, addr = 0x%08x, data = 0x%08x, len = %d\n", addr, data, len);
+    printf("\033[0;33mWrite Memory, addr = 0x%08x, data = 0x%08x, len = %d\033[0m\n", addr, data, len);
   #endif
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
