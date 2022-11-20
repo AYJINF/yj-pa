831e1f603a11a7c54b84d4f46830005d8074a9b4
tracer-ics2022 <tracer@njuics.org>
2022-11-20 11:59:24 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:59:24 up  1:06,  1 user,  load average: 0.39, 0.37, 0.35

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -58,11 +58,17 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
+  #ifdef CONFIG_DTRACE
+    printf("\033[0;33mDevice Read  Map \"%s\", addr = 0x%08x, data = 0x%08x, len = %d\033[0m\n", map->name, addr, ret, len);
+  #endif
   return ret;
 }
 
 void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   assert(len >= 1 && len <= 8);
+  #ifdef CONFIG_DTRACE
+    printf("\033[0;33mDevice Write Map \"%s\", addr = 0x%08x, data = 0x%08x, len = %d\033[0m\n", map->name, addr, data, len);
+  #endif
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
