c72888670ff0822d9a661a62e5ef406f6494a13a
tracer-ics2022 <tracer@njuics.org>
2022-12-08 21:58:38 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:58:38 up  7:37,  1 user,  load average: 0.27, 0.29, 0.32

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -18,6 +18,9 @@
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   cpu.mepc = epc;
   cpu.mcause = NO;
+  #ifdef CONFIG_ETRACE
+    printf("There is a NO.0x%08x exception at pc = 0x%08x !\n", NO, epc);
+  #endif
   return cpu.mtvec;
 }
 
