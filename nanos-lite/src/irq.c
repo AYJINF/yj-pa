4bdd5ae12816f10e6c1227a3b62a89fda25b3e4e
tracer-ics2022 <tracer@njuics.org>
2022-12-08 21:29:34 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:29:34 up  7:08,  1 user,  load average: 0.40, 0.38, 0.30

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,6 +2,7 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
+    case 1: printf("There is a YIELD event!\n"); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
