0c6a73788eb7bd27cd8cb752ec6635d5ce22d9f9
tracer-ics2022 <tracer@njuics.org>
2022-12-10 16:26:36 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:26:36 up  3:10,  1 user,  load average: 0.29, 0.37, 0.44

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -8,7 +8,6 @@ void init_fs(void);
 void init_proc(void);
 
 int main() {
-  printf("zyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy\n");
   extern const char logo[];
   printf("%s", logo);
   Log("'Hello World!' from Nanos-lite");
