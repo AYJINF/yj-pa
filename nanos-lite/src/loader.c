b36eea43d38170ee19f5a986a419a3300518057c
tracer-ics2022 <tracer@njuics.org>
2022-12-10 15:04:16 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:04:16 up  1:48,  1 user,  load average: 0.59, 0.48, 0.36

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -37,7 +37,6 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
     // fseek(elf, elf_ehdr.e_phoff + elf_ehdr.e_phentsize * i, SEEK_SET);
     // fread(&elf_phdr, sizeof(elf_phdr), 1, elf);
     ramdisk_read(&elf_phdr, elf_ehdr.e_phoff + elf_ehdr.e_phentsize * i, sizeof(elf_phdr));
-    printf("???zyy\n");
 
     
     if(elf_phdr.p_type == PT_LOAD){
