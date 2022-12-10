f786a9d14f8ba110b252e8a604f1b222798d7dea
tracer-ics2022 <tracer@njuics.org>
2022-12-10 16:47:13 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:47:13 up  3:31,  1 user,  load average: 0.35, 0.46, 0.45

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -8,7 +8,9 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      case 1: ev.event = EVENT_YIELD; break;
+      case 1: if(c->GPR1 == -1) ev.event = EVENT_YIELD;
+            else ev.event = EVENT_ERROR;
+        break;
       default: ev.event = EVENT_ERROR; break;
     }
 
