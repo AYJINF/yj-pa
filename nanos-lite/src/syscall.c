3fc32835a6fcd7db305340c42477d0f7327ea0d9
tracer-ics2022 <tracer@njuics.org>
2022-12-13 16:11:03 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:11:03 up  1:40,  1 user,  load average: 0.41, 0.44, 0.39

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -23,6 +23,7 @@ void sys_write(Context *c){
       ch++;
     }
     c->GPRx = len;
+    printf("len = %d\n", len);
   }
 }
 
@@ -36,7 +37,7 @@ void do_syscall(Context *c) {
   switch (a[0]) {
     case 0: sys_exit(c); break;
     case 1: sys_yield(c); break;
-    case 4: sys_write(c); printf("There is a zyyyyyyyyyyyyyyyyy event!\n"); break;
+    case 4: sys_write(c); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
