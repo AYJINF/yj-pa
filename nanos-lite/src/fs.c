1c38171205ceb7cae2acfce1a174ff95b2f388aa
tracer-ics2022 <tracer@njuics.org>
2022-12-14 01:45:45 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:45:45 up 11:14,  1 user,  load average: 0.29, 0.33, 0.35

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -58,7 +58,7 @@ size_t fs_read(int fd, void *buf, size_t len){
   if(f.open_offset == f.size) return 0;
   size_t ret = 0;
   if(f.read){
-    if(len > f.size - f.open_offset&& (void *)f.read == (void *)ramdisk_read) len = f.size - f.open_offset; // 阿巴阿巴
+    if(len > f.size - f.open_offset) len = f.size - f.open_offset; // 阿巴阿巴
     ret = f.read(buf, f.disk_offset + f.open_offset, len);
     f.open_offset += ret;
     return ret;
@@ -71,7 +71,7 @@ size_t fs_write(int fd, const void *buf, size_t len){
   if(f.open_offset == f.size) return 0;
   size_t ret = 0;
   if(f.write){
-    if(len > f.size - f.open_offset && (void *)f.write == (void *)ramdisk_write) len = f.size - f.open_offset; // 阿巴阿巴
+    if(len > f.size - f.open_offset) len = f.size - f.open_offset; // 阿巴阿巴
     ret = f.write(buf, f.disk_offset + f.open_offset, len);
     f.open_offset += ret;
     return ret;
