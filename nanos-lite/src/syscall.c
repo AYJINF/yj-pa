3df8e755f0b97848236cededddea3a18593d6f42
tracer-ics2022 <tracer@njuics.org>
2022-12-13 16:17:06 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:17:06 up  1:46,  1 user,  load average: 0.66, 0.49, 0.41

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -16,7 +16,6 @@ void sys_write(Context *c){
   int o_or_e = (int)c->GPR2;
   char *ch = (char *)c->GPR3;
   int len = (int)c->GPR4;
-  // printf("o_or_e = %d, len = %d\n", o_or_e, len);
   if(o_or_e == 1 || o_or_e == 2){
     for(int i = 0; i < len; i++){
       putch(*ch);
@@ -37,7 +36,7 @@ void do_syscall(Context *c) {
   switch (a[0]) {
     case 0: sys_exit(c); break;
     case 1: sys_yield(c); break;
-    case 4: sys_write(c); break;
+    case 4: sys_write(c); printf("???????????????????????\n"); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
