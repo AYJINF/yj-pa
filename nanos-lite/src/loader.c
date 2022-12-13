d81e535a3984741454617fa4bc099dacccc40cb1
tracer-ics2022 <tracer@njuics.org>
2022-12-14 01:18:53 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:18:53 up 10:47,  1 user,  load average: 0.62, 0.41, 0.38

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,6 @@
 #include <proc.h>
 #include <elf.h>
+#include <fs.h>
 
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
@@ -9,11 +10,10 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
-size_t ramdisk_read(void *buf, size_t offset, size_t len);
-
 static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Ehdr elf_ehdr;
-  ramdisk_read(&elf_ehdr, 0, sizeof(elf_ehdr));
+  int elf_file = fs_open(filename, 0, 0);
+  fs_read(elf_file, &elf_ehdr, sizeof(elf_ehdr));
 
   assert(*(uint32_t *)elf_ehdr.e_ident == 0x464c457f);
 
@@ -28,15 +28,17 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   #else
   # error Unsupported ISA
   #endif
+
   assert(EXPECT_TYPE == elf_ehdr.e_machine);
 
   for(int i = 0; i < elf_ehdr.e_phnum; i++){
     Elf_Phdr elf_phdr;
-    ramdisk_read(&elf_phdr, elf_ehdr.e_phoff + elf_ehdr.e_phentsize * i, sizeof(elf_phdr));  
-    
+    fs_lseek(elf_file, elf_ehdr.e_phoff + elf_ehdr.e_phentsize * i, SEEK_SET);
+    fs_read(elf_file, &elf_phdr, sizeof(elf_phdr));
 
     if(elf_phdr.p_type == PT_LOAD){
-      ramdisk_read((void *)elf_phdr.p_vaddr, elf_phdr.p_offset, elf_phdr.p_filesz);
+      fs_lseek(elf_file, elf_phdr.p_offset, SEEK_SET);
+      fs_read(elf_file, (void *)elf_phdr.p_vaddr, elf_phdr.p_filesz);
       memset((void *)elf_phdr.p_vaddr + elf_phdr.p_filesz, 0, elf_phdr.p_memsz - elf_phdr.p_filesz);
     }
   }
--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -2,7 +2,6 @@
 #include <assert.h>
 
 int main() {
-  printf("PASS!!!\n");
   FILE *fp = fopen("/share/files/num", "r+");
   assert(fp);
 
