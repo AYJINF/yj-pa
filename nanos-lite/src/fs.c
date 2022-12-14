5781e2be2145d23ce2191e67b1b177d1d4652916
tracer-ics2022 <tracer@njuics.org>
2022-12-14 17:03:02 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:03:02 up  2:22,  1 user,  load average: 0.27, 0.30, 0.26

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -27,24 +27,13 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
   return 0;
 }
 
-// size_t fs_serial_write(const void *buf, size_t offset, size_t len){
-//   size_t ret = len;
-//   char *c = (char *)buf;
-//   while(*c != '\0' && len--){
-//     putch(*c++);
-//   }
-//   return ret;
-// }
-
-size_t fs_serial_write(const void *buf, size_t offset, size_t len) {
-  int res = len;
-  char *now = (char *)buf;
-  while(*now != '\0' && res){
-    putch(*now);
-    now++;
-    res--;
+size_t fs_serial_write(const void *buf, size_t offset, size_t len){
+  size_t ret = len;
+  char *c = (char *)buf;
+  while(*c != '\0' && len--){
+    putch(*c++);
   }
-  return len;
+  return ret;
 }
 
 /* This is the information about all files in disk. */
