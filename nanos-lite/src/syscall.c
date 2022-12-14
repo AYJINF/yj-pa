5c3572be858876ba8d79237a70b8d6f2fe5a7d2e
tracer-ics2022 <tracer@njuics.org>
2022-12-14 20:25:31 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:25:31 up  5:44,  1 user,  load average: 0.00, 0.06, 0.15

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -82,6 +82,7 @@ void do_syscall(Context *c) {
     case SYS_close: sys_close(c); break;
     case SYS_lseek: sys_lseek(c); break;
     case SYS_brk: sys_brk(c); break;
+    case SYS_gettimeofday: sys_gettimeofday(c); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
