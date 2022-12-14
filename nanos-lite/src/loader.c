396d4ddf2b78f5bc4d2c04265d6baf82bd3224b5
tracer-ics2022 <tracer@njuics.org>
2022-12-14 15:10:11 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:10:11 up 29 min,  1 user,  load average: 0.32, 0.38, 0.31

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -31,12 +31,14 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 
   assert(EXPECT_TYPE == elf_ehdr.e_machine);
 
+
+  size_t ph_offest = elf_ehdr.e_phoff;
   for(int i = 0; i < elf_ehdr.e_phnum; i++){
     Elf_Phdr elf_phdr;
-    fs_lseek(elf_file, elf_ehdr.e_phoff + elf_ehdr.e_phentsize * i, SEEK_SET);
-    fs_read(elf_file, &elf_phdr, sizeof(elf_phdr));printf("jb\n");
+    fs_lseek(elf_file, ph_offest + elf_ehdr.e_phentsize * i, SEEK_SET);
+    fs_read(elf_file, &elf_phdr, sizeof(elf_phdr));
 
-    if(elf_phdr.p_type == PT_LOAD){
+    if(elf_phdr.p_type == PT_LOAD){printf("jb\n");
       fs_lseek(elf_file, elf_phdr.p_offset, SEEK_SET);
       fs_read(elf_file, (void *)elf_phdr.p_vaddr, elf_phdr.p_filesz);
       memset((void *)elf_phdr.p_vaddr + elf_phdr.p_filesz, 0, elf_phdr.p_memsz - elf_phdr.p_filesz);
