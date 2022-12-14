3f442319ee62efbb9628881d7b371d9f96906392
tracer-ics2022 <tracer@njuics.org>
2022-12-14 22:57:45 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:57:45 up  8:17,  1 user,  load average: 0.52, 0.31, 0.29

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -4,12 +4,16 @@
 #include <string.h>
 #include <unistd.h>
 
+#include <sys/time.h>
+
 static int evtdev = -1;
 static int fbdev = -1;
 static int screen_w = 0, screen_h = 0;
 
 uint32_t NDL_GetTicks() {
-  return 0;
+  struct timeval tval;
+  gettimeofday(&tval, NULL);
+  return tval.tv_sec * 1000 + tval.tv_usec / 1000;
 }
 
 int NDL_PollEvent(char *buf, int len) {
--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <sys/time.h>
+#include <NDL.h>
 #include <assert.h>
 
-int main() {
-  struct timeval tval;
-  int ret = gettimeofday(&tval, NULL);
-  if(ret == -1) assert(0);
-  int t0 = tval.tv_sec * 10 + tval.tv_usec / 100000;
-  while (1)
-  {
-    gettimeofday(&tval, NULL);
-    int t1 = tval.tv_sec * 10 + tval.tv_usec / 100000;
-    // printf("t0 = %d, t1 = %d\n", t0 / 100000, t1 / 100000);
-    if(t1-t0 == 5){
-        printf("t0 = %d, t1 = %d\n", t0, t1);
-        t0 = t1;
+// int main() {
+//   struct timeval tval;
+//   int ret = gettimeofday(&tval, NULL);
+//   if(ret == -1) assert(0);
+//   int t0 = tval.tv_sec * 10 + tval.tv_usec / 100000; // 单位为0.1s（ds）
+//   while (1)
+//   {
+//     gettimeofday(&tval, NULL);
+//     int t1 = tval.tv_sec * 10 + tval.tv_usec / 100000;
+//     if(t1-t0 == 5){
+//         printf("t0 = %d, t1 = %d\n", t0, t1);
+//         t0 = t1;
+//     }
+//   }
+// }
+int main(){
+  NDL_Init(0);
+  uint32_t t0 = NDL_GetTicks();
+  while(1){
+    uint32_t t1 = NDL_GetTicks();
+    if(t1 - t0 == 500){
+      printf("t0 = 0x%08x, t1 = 0x%08x\n", t0, t1);
+      t0 = t1;
     }
   }
+  NDL_Quit();
 }
