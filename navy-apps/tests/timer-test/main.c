e45fddf87326de7e248a4c93963d014d33a61b09
tracer-ics2022 <tracer@njuics.org>
2022-12-14 21:59:03 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:59:03 up  7:18,  1 user,  load average: 0.42, 0.50, 0.39

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -7,11 +7,11 @@ int main() {
   struct timeval tval;
   int ret = gettimeofday(&tval, NULL);
   if(ret == -1) assert(0);
-  int t0 = tval.tv_sec * 100 + tval.tv_usec / 10000;
+  int t0 = tval.tv_sec * 10 + tval.tv_usec / 100000;
   while (1)
   {
     gettimeofday(&tval, NULL);
-    int t1 = tval.tv_sec * 100 + tval.tv_usec / 10000;
+    int t1 = tval.tv_sec * 10 + tval.tv_usec / 100000;
     // printf("t0 = %d, t1 = %d\n", t0 / 100000, t1 / 100000);
     if(t1-t0 == 5){
         printf("t0 = %d, t1 = %d\n", t0 / 10, t1 / 10);
