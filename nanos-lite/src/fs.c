d161ce7100a1bb87d0c437cd35f5ea2bb7a2bd5f
tracer-ics2022 <tracer@njuics.org>
2022-12-14 15:44:51 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:44:51 up  1:04,  1 user,  load average: 0.15, 0.19, 0.23

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -69,9 +69,9 @@ size_t fs_read(int fd, void *buf, size_t len){
 size_t fs_write(int fd, const void *buf, size_t len){
   Finfo *f = &file_table[fd];
   if(f->open_offset == f->size) return 0;
-  printf("zyy\n");
   size_t ret = 0;
   if(f->write){
+    printf("zyy\n");
     if(len > f->size - f->open_offset) len = f->size - f->open_offset; // 阿巴阿巴
     ret = f->write(buf, f->disk_offset + f->open_offset, len);
     f->open_offset += ret;
