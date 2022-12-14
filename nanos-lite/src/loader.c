77802a4a8e6a6ef2dc3ed536d7465011717f837f
tracer-ics2022 <tracer@njuics.org>
2022-12-14 15:07:17 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:07:17 up 26 min,  1 user,  load average: 0.44, 0.48, 0.32

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -13,7 +13,7 @@
 static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Ehdr elf_ehdr;
   int elf_file = fs_open(filename, 0, 0);
-  fs_read(elf_file, &elf_ehdr, sizeof(elf_ehdr));printf("jb\n");
+  fs_read(elf_file, &elf_ehdr, sizeof(elf_ehdr));
 
   assert(*(uint32_t *)elf_ehdr.e_ident == 0x464c457f);
 
@@ -31,6 +31,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 
   assert(EXPECT_TYPE == elf_ehdr.e_machine);
 
+  printf("jb\n");
   for(int i = 0; i < elf_ehdr.e_phnum; i++){
     Elf_Phdr elf_phdr;
     fs_lseek(elf_file, elf_ehdr.e_phoff + elf_ehdr.e_phentsize * i, SEEK_SET);
