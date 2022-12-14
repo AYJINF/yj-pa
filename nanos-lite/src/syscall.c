79622d00ec0ed2d678330640a7d40d67ed2f8176
tracer-ics2022 <tracer@njuics.org>
2022-12-14 15:04:26 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:04:26 up 23 min,  1 user,  load average: 0.66, 0.49, 0.30

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -14,8 +14,7 @@ void sys_yield(Context *c){
 }
 
 void sys_open(Context *c){
-  printf("jb\n");
-  c->GPRx = fs_open((char *)c->GPR2, c->GPR3, c->GPR4);printf("jxb\n");
+  c->GPRx = fs_open((char *)c->GPR2, c->GPR3, c->GPR4);
 }
 
 void sys_read(Context *c){
@@ -54,7 +53,7 @@ void do_syscall(Context *c) {
   #ifdef CONFIG_STRACE
   Log("Syscall: mcause = 0x%x, GPR1 = 0x%x, GPR2 = 0x%x, GPR3 = 0x%x, GPR4 = 0x%x", c->mcause, c->GPR1, c->GPR2, c->GPR3, c->GPR4);
   #endif
-
+printf("jb\n");
   switch (a[0]) {
     case SYS_exit: sys_exit(c); break;
     case SYS_yield: sys_yield(c); break;
