a0e774c9de2b0bd992ec6cd72bb838084c873356
tracer-ics2022 <tracer@njuics.org>
2022-12-14 16:30:05 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:30:05 up  1:49,  1 user,  load average: 0.22, 0.27, 0.26

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -21,23 +21,23 @@ void sys_read(Context *c){
   c->GPRx = fs_read(c->GPR2, (void *)c->GPR3, c->GPR4);
 }
 
-// void sys_write(Context *c){
-//   int o_or_e = (int)c->GPR2;
-//   char *ch = (char *)c->GPR3;
-//   int len = c->GPR4;
-//   if(o_or_e == 1 || o_or_e == 2){
-//     for(int i = 0; i < len; i++){
-//       putch(*ch);
-//       ch++;
-//     }
-//     c->GPRx = len;
-//   }
-//   else(c->GPRx = -1);
-// }
-
 void sys_write(Context *c){
-  c->GPRx = fs_write(c->GPR2, (void *)c->GPR3, c->GPR4); // 阿巴阿巴
+  int o_or_e = (int)c->GPR2;
+  char *ch = (char *)c->GPR3;
+  int len = c->GPR4;
+  if(o_or_e == 1 || o_or_e == 2){
+    for(int i = 0; i < len; i++){
+      putch(*ch);
+      ch++;
+    }
+    c->GPRx = len;
+  }
+  else(c->GPRx = -1);
 }
+
+// void sys_write(Context *c){
+//   c->GPRx = fs_write(c->GPR2, (void *)c->GPR3, c->GPR4); // 阿巴阿巴
+// }
 
 void sys_close(Context *c){
   c->GPRx = fs_close(c->GPR2);
