647005fe03211946d8ab4eda26160e7cb2e4c616
tracer-ics2022 <tracer@njuics.org>
2022-12-15 14:49:00 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:49:00 up 57 min,  1 user,  load average: 0.63, 0.63, 0.56

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -53,11 +53,11 @@ int fs_open(const char *pathname, int flags, int mode){
 }
 
 size_t fs_read(int fd, void *buf, size_t len){
-  printf("fd=%d\n", fd);
   Finfo *f = &file_table[fd];
   if(f->open_offset == f->size) return 0;
   size_t ret = 0;
   if(f->read){
+  printf("fd=%d\n", fd);
     if(len > f->size - f->open_offset && (void *)f->read == (void *)ramdisk_read) len = f->size - f->open_offset;
     if((void *)f->read == (void *)ramdisk_read) printf("ramdisk\n");
     if((void *)f->read == (void *)events_read) printf("event\n");
