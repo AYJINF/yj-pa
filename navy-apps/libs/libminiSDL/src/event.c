e552811fb5d13e871d02f85ac414f8f7c1709e88
tracer-ics2022 <tracer@njuics.org>
2022-12-19 20:11:07 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:11:07 up 46 min,  1 user,  load average: 0.22, 0.59, 0.60

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -44,7 +44,8 @@ int SDL_WaitEvent(SDL_Event *event) {
   char k_name[16];
   int pos = 0;
   while(ndl_event[3 + pos] !='\n'){
-    k_name[pos] = ndl_event[3 + pos++]; // 阿巴阿巴
+    k_name[pos] = ndl_event[3 + pos]; // 阿巴阿巴
+    pos++;
   }
   k_name[pos] = '\0';
   for(int i = 0; i < sizeof(keyname) / sizeof(char *); i++){
