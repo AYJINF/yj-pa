3b202e595f4de3951933d49d5bfee4a05a069f74
tracer-ics2022 <tracer@njuics.org>
2022-12-20 15:51:17 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:51:17 up  2:23,  1 user,  load average: 0.39, 0.25, 0.13

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -64,7 +64,6 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
     assert(s);
     if(s->format->BitsPerPixel == 32){
-    printf("zyyyyyyyyyy\n");
     if(x ==0 && y == 0 && w == 0 && h == 0) // 阿巴阿巴
       NDL_DrawRect((uint32_t *)s->pixels, 0, 0, s->w, s->h);
     else {
