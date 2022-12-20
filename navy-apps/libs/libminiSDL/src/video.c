3150457813f37cd99cfa4f84c24ce643ba91420c
tracer-ics2022 <tracer@njuics.org>
2022-12-20 15:50:02 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:50:02 up  2:22,  1 user,  load average: 0.46, 0.22, 0.11

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -64,10 +64,10 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
     assert(s);
     if(s->format->BitsPerPixel == 32){
+    printf("zyyyyyyyyyy\n");
     if(x ==0 && y == 0 && w == 0 && h == 0) // 阿巴阿巴
       NDL_DrawRect((uint32_t *)s->pixels, 0, 0, s->w, s->h);
     else {
-    printf("zyyyyyyyyyy\n");
       uint32_t *pixels = malloc(sizeof(uint32_t) * w * h);
       for(int i = 0; i < h ; i++)
         memcpy(&pixels[i * w], &s->pixels[(i+y)*s->w + x], sizeof(uint32_t) * w); 
