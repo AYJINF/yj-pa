05a417b775ea7adaf276aa7064898b6ed805168c
tracer-ics2022 <tracer@njuics.org>
2023-01-09 23:57:35 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:57:35 up  7:18,  1 user,  load average: 1.00, 0.61, 0.36

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -25,19 +25,19 @@ void hello_fun(void *arg) {
 }
 
 void init_proc() {
-  context_kload(&pcb[0], hello_fun, (void *)"ab");
+  // context_kload(&pcb[0], hello_fun, (void *)"ab");
   // context_kload(&pcb[1], hello_fun, (void *)"cd");
   // printf("zyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy\n");
   // context_uload(&pcb[0], "/bin/hello");
-  char *argv[] = {"--skip", NULL};
-  context_uload(&pcb[1], "/bin/pal", argv, NULL);
+  // char *argv[] = {"--skip", NULL};
+  // context_uload(&pcb[1], "/bin/pal", argv, NULL);
 
   switch_boot_pcb();
 
   Log("Initializing processes...");
 
   // load program here
-  // naive_uload(NULL, "/bin/nterm");
+  naive_uload(NULL, "/bin/exec-test");
 
 }
 
@@ -51,3 +51,10 @@ current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
 // then return the new context
 return current->cp;
 }
+
+int execve(const char *filename, char *const argv[], char *const envp[]){
+  context_uload(&pcb[1], filename, argv, envp);
+  switch_boot_pcb();
+  yield();
+  return 0;
+};
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -16,6 +16,7 @@ char *files[] = {"stdin", "stdout", "stderr", "/dev/events", "/dev/fb", "/proc/d
 #endif
 
 extern void naive_uload(PCB *pcb, const char *filename);
+int execve(const char *filename, char *const argv[], char *const envp[]);
 
 void sys_exit(Context *c){
   naive_uload(NULL, "/bin/nterm");
@@ -52,8 +53,7 @@ void sys_brk(Context *c){
 }
 
 void sys_execve(Context *c){
-  naive_uload(NULL, (char *)c->GPR2);
-  c->GPRx = 0;
+  c->GPRx = execve((char *)c->GPR2, (char **)c->GPR3, (char **)c->GPR4);
 }
 
 void sys_gettimeofday(Context *c){
@@ -67,6 +67,9 @@ void sys_gettimeofday(Context *c){
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
 
   #ifdef CONFIG_STRACE
   switch (a[0])
