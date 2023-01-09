947a85488f90864721134060e1561cad5ea456af
tracer-ics2022 <tracer@njuics.org>
2023-01-09 21:56:17 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:56:17 up  5:17,  1 user,  load average: 0.83, 0.43, 0.29

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -29,7 +29,7 @@ void init_proc() {
   // context_kload(&pcb[1], hello_fun, (void *)"cd");
   // printf("zyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy\n");
   // context_uload(&pcb[0], "/bin/hello");
-  char *argv[] = {NULL};
+  char *argv[] = {"--skip"};
   context_uload(&pcb[1], "/bin/pal", argv, NULL);
 
   switch_boot_pcb();
