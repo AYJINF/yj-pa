0a07b0d256026be26c5a60164e28ece6cbb0d4b5
tracer-ics2022 <tracer@njuics.org>
2023-01-10 00:27:23 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:27:23 up  7:48,  1 user,  load average: 1.02, 0.58, 0.50

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -64,6 +64,7 @@ void context_kload(PCB *pcb, void (*entry)(void *), void *arg){
 void context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[]){
   // char *string_area = (char *)heap.end;
   char *string_area = (char *)new_page(8);
+  printf("ydddddddddddddddddddddddddddd\n");
   int argv_num = 0, envp_num = 0;
   if(argv) while(argv[argv_num]) argv_num++;
   if(envp) while(envp[envp_num]) envp_num++;
@@ -83,7 +84,6 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
     assert(t == envp[i]); // test
   }
   uintptr_t *string_a = (uintptr_t *)string_area;
-  printf("ydddddddddddddddddddddddddddd\n");
   string_a--; *string_a = (uintptr_t)NULL;
 
   for(int i = envp_num - 1; i >= 0; i--){
