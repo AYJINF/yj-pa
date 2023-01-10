8a3dc59ea9b2c16ccc93e07b8cfc88eebb4e994a
tracer-ics2022 <tracer@njuics.org>
2023-01-10 16:52:07 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:52:07 up 1 day, 13 min,  1 user,  load average: 0.55, 0.42, 0.33

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -64,6 +64,7 @@ void context_kload(PCB *pcb, void (*entry)(void *), void *arg){
 void context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[]){
   // char *string_area = (char *)heap.end;
   char *string_area = (char *)new_page(8);
+  printf("wwwww\n");
   int argv_num = 0, envp_num = 0;
   if(argv) while(argv[argv_num]) argv_num++;
   if(envp) while(envp[envp_num]) envp_num++;
--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -57,7 +57,6 @@ return current->cp;
 
 int execve(const char *filename, char *const argv[], char *const envp[]){
   context_uload(&pcb[1], filename, argv, envp);
-  printf("wwwww\n");
   switch_boot_pcb();
   yield();
   return 0;
