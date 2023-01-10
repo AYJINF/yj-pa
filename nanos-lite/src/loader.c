9e4ae6cd97725f97a3cf76574f9f565b7b8f4f8a
tracer-ics2022 <tracer@njuics.org>
2023-01-10 16:54:20 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:54:20 up 1 day, 15 min,  1 user,  load average: 0.45, 0.45, 0.36

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -70,13 +70,12 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   // printf("argv_num=%d, envp_num=%d\n", argv_num, envp_num);
   char *argv_c[argv_num];
   char *envp_c[envp_num];
-  for(int i = 0; i < argv_num; i++){
+  if(argv) for(int i = 0; i < argv_num; i++){
     string_area -= ROUNDUP(strlen(argv[i]) + 1, 4); // +1 for the '\0'
     strcpy(string_area, argv[i]);
     argv_c[i] = string_area;
   }
-  for(int i = 0; i < envp_num; i++){
-  printf("wwwww\n");
+  if(envp) for(int i = 0; i < envp_num; i++){
     string_area -= ROUNDUP(strlen(envp[i]) + 1, 4); // +1 for the '\0'
     char *t = string_area; // test
     strcpy(string_area, envp[i]);
