02b46a2b02f4c6cd2b6a92b843612e4f6952f26b
tracer-ics2022 <tracer@njuics.org>
2023-01-10 17:43:20 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:43:20 up 1 day,  1:04,  1 user,  load average: 0.41, 0.31, 0.27

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -1,13 +1,11 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <assert.h>
-#include <stdio.h>
 
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
   int argc = (int)*args++;
-  printf("argc=%d\n", argc);  
   char *args_c = (char *)args;
   char **argv = (char **)args_c;
   while(*args_c) args_c++;
