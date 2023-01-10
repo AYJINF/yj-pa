466f064661917df551205cdfb1f08fa3db709f00
tracer-ics2022 <tracer@njuics.org>
2023-01-10 20:15:05 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:15:05 up 1 day,  3:35,  1 user,  load average: 0.58, 0.38, 0.19

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -57,6 +57,7 @@ return current->cp;
 
 int execve(const char *filename, char *const argv[], char *const envp[]){
   context_uload(&pcb[1], filename, argv, envp);
+  printf("filename=%s\n", filename);
   switch_boot_pcb();
   yield();
   return 0;
