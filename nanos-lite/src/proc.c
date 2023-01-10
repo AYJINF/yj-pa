7773755979e17c4156aba03e0eaaadcde050c274
tracer-ics2022 <tracer@njuics.org>
2023-01-10 16:31:58 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:31:58 up 23:52,  1 user,  load average: 0.52, 0.49, 0.33

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -46,9 +46,9 @@ Context* schedule(Context *prev) {
 current->cp = prev;
 
 // always select pcb[0] as the new process
-current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
-bool flag = (current == &pcb[0]);
-if(flag) printf("dddddddddddddd\n");
+current = (current == &pcb[1] ? &pcb[0] : &pcb[1]);
+// bool flag = (current == &pcb[0]);
+// if(flag) printf("dddddddddddddd\n");
 
 // then return the new context
 return current->cp;
