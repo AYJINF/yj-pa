47b8ff343e114c11b4d0cee4b11a0b5d814ed09e
tracer-ics2022 <tracer@njuics.org>
2023-01-11 00:07:55 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:07:55 up 12 min,  1 user,  load average: 0.78, 0.50, 0.29

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -90,8 +90,8 @@ int NDL_QueryAudio() {
 }
 
 int NDL_Init(uint32_t flags) {
+  if (getenv("NWM_APP")) printf("3333333333\n");
   if (getenv("NWM_APP")) {
-  printf("gggggggggggggggg\n");
     evtdev = 3;
   }
   return 0;
