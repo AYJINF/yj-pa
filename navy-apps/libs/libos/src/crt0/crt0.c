4ec9d53d5fb0768e3e9c56e8def4816b8be9c15b
tracer-ics2022 <tracer@njuics.org>
2023-01-10 22:33:51 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:33:51 up 1 day,  5:54,  1 user,  load average: 0.48, 0.46, 0.33

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -12,7 +12,8 @@ void call_main(uintptr_t *args) {
   while(*args_c) args_c++;
   args_c++;
   char **envp = (char **)args_c;
-  if(envp) printf("envp[0]=%s", envp[0]);
+  printf("argc=%d\n", argc);
+  // if(envp) printf("envp[0]=%s", envp[0]);
   environ = envp;
   exit(main(argc, argv, envp));
   assert(0);
