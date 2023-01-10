3bb153afeee32b842a0ceac485c9de6719520e6f
tracer-ics2022 <tracer@njuics.org>
2023-01-10 17:05:55 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:05:55 up 1 day, 26 min,  1 user,  load average: 0.33, 0.56, 0.49

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -56,7 +56,11 @@ void sys_brk(Context *c){
 void sys_execve(Context *c){
   // naive_uload(NULL, (char *)c->GPR2);
   // c->GPRx = 0;
-  c->GPRx = execve((char *)c->GPR2, (char **)c->GPR3, (char **)c->GPR4);
+  char *filename = (char *)c->GPR2;
+  char **argv = (char **)c->GPR3;
+  char **envp = (char **)c->GPR4;
+  c->GPRx = execve(filename, argv, envp); 
+  // c->GPRx = execve((char *)c->GPR2, (char **)c->GPR3, (char **)c->GPR4);
 }
 
 void sys_gettimeofday(Context *c){
