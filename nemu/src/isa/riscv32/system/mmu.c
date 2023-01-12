7dada9d5c91a54f3baae8fccd3b4ae730d1571af
tracer-ics2022 <tracer@njuics.org>
2023-01-13 01:42:12 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:42:12 up 1 day,  5:54,  1 user,  load average: 0.41, 0.35, 0.33

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -66,12 +66,12 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
    // 处理pde
   paddr_t pde_addr = ((cpu.satp & MY_SATP_PPN) << 12) | ((vaddr & MY_VPN_1) >> 22) * 4;
   uintptr_t pde = paddr_read(pde_addr, 4);
-  assert((pde & PTE_V) != 0);
+  Assert((pde & PTE_V) != 0, "pde goes wrong in mmu.c!");
   
   // 处理pte
   paddr_t pte_addr = (((pde & ~MY_PDE_ATT) >> 10) << 12) | ((vaddr & MY_VPN_0) >> 12) * 4;
   uintptr_t pte = paddr_read(pte_addr, 4);
-  assert((pte & PTE_V) != 0);
+  Assert((pte & PTE_V) != 0, "pte goes wrong in mmu.c!");
 
   switch (type)
   {
--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -17,20 +17,28 @@
 #include <memory/paddr.h>
 
 word_t vaddr_ifetch(vaddr_t addr, int len) {
-  if(isa_mmu_check(addr, len, 0) == MMU_TRANSLATE)
-  assert(0);
+  if(isa_mmu_check(addr, len, 0) == MMU_TRANSLATE){
+    vaddr_t tmp = addr;
     addr = isa_mmu_translate(addr, len, 0);
+    Assert( tmp == addr, "不是恒等映射！");
+  }
   return paddr_read(addr, len);
 }
 
 word_t vaddr_read(vaddr_t addr, int len) {
-  if(isa_mmu_check(addr, len, 0) == MMU_TRANSLATE)
+  if(isa_mmu_check(addr, len, 0) == MMU_TRANSLATE){
+    vaddr_t tmp = addr;
     addr = isa_mmu_translate(addr, len, 0);
+    Assert( tmp == addr, "不是恒等映射！");
+  }
   return paddr_read(addr, len);
 }
 
 void vaddr_write(vaddr_t addr, int len, word_t data) {
-  if(isa_mmu_check(addr, len, 1) == MMU_TRANSLATE)
+  if(isa_mmu_check(addr, len, 1) == MMU_TRANSLATE){
+    vaddr_t tmp = addr;
     addr = isa_mmu_translate(addr, len, 0);
+    Assert( tmp == addr, "不是恒等映射！");
+  }
   paddr_write(addr, len, data);
 }
