4fa11b256640dbc5269cb618cac6d38d87d49a24
tracer-ics2022 <tracer@njuics.org>
2023-01-13 19:33:00 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:33:00 up  6:40,  1 user,  load average: 0.54, 0.47, 0.43

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -46,6 +46,7 @@ bool vme_init(void* (*pgalloc_f)(int), void (*pgfree_f)(void*)) {
 
 void protect(AddrSpace *as) {
   PTE *updir = (PTE*)(pgalloc_usr(PGSIZE));
+  printf("hhhhhhhh\n");
   as->ptr = updir;
   as->area = USER_SPACE;
   as->pgsize = PGSIZE;
