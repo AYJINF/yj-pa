3ace8b1dd3fd0af44d126d7c99adce67de49c75b
tracer-ics2022 <tracer@njuics.org>
2023-01-13 18:35:30 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:35:30 up  5:43,  1 user,  load average: 0.23, 0.26, 0.20

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -29,7 +29,7 @@ void init_proc() {
   char *argv[] = {NULL};
   context_uload(&pcb[1], "/bin/dummy", argv, NULL);
   // context_kload(&pcb[1], hello_fun, (void *)"cd");
-  // printf("zyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy\n");
+  printf("zyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy\n");
   // context_uload(&pcb[0], "/bin/hello");
 
   // context_uload(&pcb[0], "/bin/hello", NULL, NULL);
@@ -37,7 +37,6 @@ void init_proc() {
 
   // char *argv[] = {"--skip", NULL};
   // context_uload(&pcb[1], "/bin/pal", argv, NULL);
-
   switch_boot_pcb();
 
   Log("Initializing processes...");
