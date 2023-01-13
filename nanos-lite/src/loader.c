4d6e7798477562f505d025308583bc2a1754cc1d
tracer-ics2022 <tracer@njuics.org>
2023-01-13 17:17:43 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:17:43 up  4:25,  1 user,  load average: 0.55, 0.62, 0.45

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -71,7 +71,7 @@ void context_kload(PCB *pcb, void (*entry)(void *), void *arg){
 
 // void context_uload(PCB *pcb, const char *filename){
 void context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[]){
-  // protect(&pcb->as);
+  protect(&pcb->as);
   int pgsize = pcb->as.pgsize;
   char *string_area = (char *)new_page(8) + 8 * pgsize;
 
