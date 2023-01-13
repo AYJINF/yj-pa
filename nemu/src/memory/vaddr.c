2db00fc40772bc9cff0ab4e530889732b7e5949c
tracer-ics2022 <tracer@njuics.org>
2023-01-13 21:33:13 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:33:13 up  8:40,  1 user,  load average: 0.98, 0.46, 0.31

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -17,7 +17,7 @@
 #include <memory/paddr.h>
 
 word_t vaddr_ifetch(vaddr_t addr, int len) {
-  printf("vaddr_ifetch, addr=%d\n", addr);
+  printf("vaddr_ifetch, addr=%u\n", addr);
   if(isa_mmu_check(addr, len, 0) == MMU_TRANSLATE){
     vaddr_t tmp = addr;
     addr = isa_mmu_translate(addr, len, 0);
@@ -28,7 +28,7 @@ word_t vaddr_ifetch(vaddr_t addr, int len) {
 }
 
 word_t vaddr_read(vaddr_t addr, int len) {
-  printf("vaddr_read, addr=%d\n", addr);
+  printf("vaddr_read, addr=%u\n", addr);
   if(isa_mmu_check(addr, len, 0) == MMU_TRANSLATE){
     vaddr_t tmp = addr;
     addr = isa_mmu_translate(addr, len, 0);
@@ -39,7 +39,7 @@ word_t vaddr_read(vaddr_t addr, int len) {
 }
 
 void vaddr_write(vaddr_t addr, int len, word_t data) {
-  printf("vaddr_write, addr=%d\n", addr);
+  printf("vaddr_write, addr=%u\n", addr);
   if(isa_mmu_check(addr, len, 1) == MMU_TRANSLATE){
     vaddr_t tmp = addr;
     addr = isa_mmu_translate(addr, len, 0);
