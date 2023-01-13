515ca18138893f773df3602fb1fa6594246628c1
tracer-ics2022 <tracer@njuics.org>
2023-01-13 15:18:10 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:18:10 up  2:25,  1 user,  load average: 0.59, 0.40, 0.31

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -38,12 +38,12 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
     fs_read(elf_file, &elf_phdr, sizeof(elf_phdr));
 
     if(elf_phdr.p_type == PT_LOAD){
-      int pgsize = PGSIZE;
-      size_t nr_page = (elf_phdr.p_memsz - 1) * pgsize + 1;
-      void *p_pages = new_page(nr_page);
-      for(int j = 0; j < nr_page; j++){
-        map(&pcb->as, (void *)((elf_phdr.p_vaddr & (pgsize - 1)) + j * pgsize), (void *)(p_pages + j * pgsize), 1);
-      }
+      // int pgsize = PGSIZE;
+      // size_t nr_page = (elf_phdr.p_memsz - 1) * pgsize + 1;
+      // void *p_pages = new_page(nr_page);
+      // for(int j = 0; j < nr_page; j++){
+      //   map(&pcb->as, (void *)((elf_phdr.p_vaddr & (pgsize - 1)) + j * pgsize), (void *)(p_pages + j * pgsize), 1);
+      // }
       fs_lseek(elf_file, elf_phdr.p_offset, SEEK_SET);
       fs_read(elf_file, (void *)elf_phdr.p_vaddr, elf_phdr.p_filesz);
       memset((void *)elf_phdr.p_vaddr + elf_phdr.p_filesz, 0, elf_phdr.p_memsz - elf_phdr.p_filesz);
