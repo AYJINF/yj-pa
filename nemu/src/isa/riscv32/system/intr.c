3304fdc86b021b02357ae7bd7b68405f976a7bec
tracer-ics2022 <tracer@njuics.org>
2023-01-13 18:42:11 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:42:11 up  5:49,  1 user,  load average: 0.68, 0.57, 0.37

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -16,7 +16,7 @@
 #include <isa.h>
 
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
-  printf("ooooooooooooooooooo\n");
+  printf("NO=%d\n", NO);
   cpu.mepc = epc;
   cpu.mcause = NO;
   #ifdef CONFIG_ETRACE
