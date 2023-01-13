ced55b1a094064fd46d2493fcb808b9400af329d
tracer-ics2022 <tracer@njuics.org>
2023-01-13 20:08:38 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:08:38 up  7:16,  1 user,  load average: 0.32, 0.29, 0.30

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -9,6 +9,7 @@ void __am_switch(Context *c);
 Context* __am_irq_handle(Context *c) {
   printf("pdir=%p\n", c->pdir);
   __am_get_cur_as(c);
+  printf("pdir2=%p\n", c->pdir);
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
