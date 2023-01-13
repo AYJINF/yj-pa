13f3e5f64956bd8487e688c85653dd3d891521c3
tracer-ics2022 <tracer@njuics.org>
2023-01-14 01:35:15 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:35:15 up 12:42,  1 user,  load average: 1.20, 0.76, 0.50

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -44,6 +44,7 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   ret->mstatus = 0x18000;
   ret->mepc = (uintptr_t)entry;
   ret->GPRx = (uintptr_t)arg;
+  ret->pdir = NULL;
   return ret;
 }
 
