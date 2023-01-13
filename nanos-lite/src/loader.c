f91c8df286fcccd6a41ec9ad2a840d32f2b2f20b
tracer-ics2022 <tracer@njuics.org>
2023-01-13 23:25:47 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:25:47 up 10:33,  1 user,  load average: 0.46, 0.37, 0.28

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -138,6 +138,6 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   kstack.start = &pcb->cp;
   kstack.end = kstack.start + STACK_SIZE;
   pcb->cp = ucontext(&pcb->as, kstack, (void *)loader(pcb, filename));
-  pcb->cp->GPRx = (uintptr_t)string_a;
+  pcb->cp->GPRx = (uintptr_t)string_a; // 可能会错
 }
 
