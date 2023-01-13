bbf76b874198070abf8636fb0d9ee99492a926d6
tracer-ics2022 <tracer@njuics.org>
2023-01-13 19:18:23 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:18:23 up  6:26,  1 user,  load average: 0.67, 0.46, 0.32

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -108,6 +108,7 @@ Context *ucontext(AddrSpace *as, Area kstack, void *entry) {
   Context *ret = kstack.end - sizeof(Context);
   ret->mstatus = 0x18000;
   ret->mepc = (uintptr_t)entry;
+  printf("ucontext pdir=%d\n", as->ptr);
   ret->pdir = as->ptr;
   return ret;
 }
