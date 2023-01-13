23f81291eb4cf3d5c167d4706320ed82bc1915be
tracer-ics2022 <tracer@njuics.org>
2023-01-13 17:21:17 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:21:17 up  4:28,  1 user,  load average: 0.74, 0.70, 0.52

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -27,7 +27,7 @@ void hello_fun(void *arg) {
 void init_proc() {
   context_kload(&pcb[0], hello_fun, (void *)"ab");
   char *argv[] = {NULL};
-  context_uload(&pcb[1], "/bin/nterm", argv, NULL);
+  context_uload(&pcb[1], "/bin/dummy", argv, NULL);
   // context_kload(&pcb[1], hello_fun, (void *)"cd");
   // printf("zyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy\n");
   // context_uload(&pcb[0], "/bin/hello");
