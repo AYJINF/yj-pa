68d47cd70009ae23f09855c8b3bc37759cb4b97a
tracer-ics2022 <tracer@njuics.org>
2023-01-13 18:49:50 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:49:50 up  5:57,  1 user,  load average: 0.49, 0.52, 0.42

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -6,7 +6,6 @@ Context* schedule(Context *prev);
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case 1: 
-    printf("pdir=%d\n", c->pdir);
       // printf("There is a YIELD event!\n"); 
       return schedule(c);
       // printf("There is a YIELD event!\n"); break; // 自陷指令
