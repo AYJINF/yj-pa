145934056af42845fa5f606166688cd9dd868785
tracer-ics2022 <tracer@njuics.org>
2023-01-13 18:50:19 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:50:19 up  5:58,  1 user,  load average: 0.63, 0.55, 0.44

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -7,7 +7,6 @@ void __am_get_cur_as(Context *c);
 void __am_switch(Context *c);
 
 Context* __am_irq_handle(Context *c) {
-  printf("ggggggggggggg\n");
   __am_get_cur_as(c);
   if (user_handler) {
     Event ev = {0};
@@ -23,7 +22,7 @@ Context* __am_irq_handle(Context *c) {
     assert(c != NULL);
     c->mepc += 4; // 时钟中断不用+4
   }
-
+  printf("ggggggggggggg\n");
   __am_switch(c);
   return c;
 }
