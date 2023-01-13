fbd297ec2f3e0fd32f24dbc606c6e05ff19afb83
tracer-ics2022 <tracer@njuics.org>
2023-01-13 23:17:58 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:17:58 up 10:25,  1 user,  load average: 0.09, 0.12, 0.17

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -39,7 +39,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 
     if(elf_phdr.p_type == PT_LOAD){
       int pgsize = PGSIZE;
-      size_t nr_page = (elf_phdr.p_memsz) / pgsize + 1;
+      size_t nr_page = elf_phdr.p_memsz / pgsize + 1; // 阿巴阿巴
       void *p_pages = new_page(nr_page);
       for(int j = 0; j < nr_page; j++){
         // printf("lllllllllllllllllllllloader va=%p, pa=%p\n", (void *)((elf_phdr.p_vaddr & (~(pgsize - 1))) + j * pgsize), (void *)(p_pages + j * pgsize));
