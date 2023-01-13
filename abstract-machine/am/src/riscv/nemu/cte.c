c6a4d657d8cf4a01f2caf1bf26304fab1c78121d
tracer-ics2022 <tracer@njuics.org>
2023-01-13 19:12:30 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:12:30 up  6:20,  1 user,  load average: 0.77, 0.31, 0.24

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -47,8 +47,8 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
 }
 
 void yield() {
-  asm volatile("li a7, -1; ecall");
   printf("jjjjjjjjjjj\n");
+  asm volatile("li a7, -1; ecall");
 }
 
 bool ienabled() {
