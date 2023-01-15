fecdd84e0855159f31035635a5b68047167b9e58
tracer-ics2022 <tracer@njuics.org>
2023-01-15 13:20:49 +0800

    >  compile NEMU 211240074 余今 Linux yj-Yoga 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:20:49 up 21:47,  1 user,  load average: 1.03, 0.57, 0.50

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -40,11 +40,11 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
 }
 
 word_t isa_query_intr() {
-  // if (cpu.INTR) {
-  //   if(cpu.mstatus & MIE){ // 否则CPU处于关中断状态
-  //   cpu.INTR = false;
-  //   return IRQ_TIMER;
-  //   }
-  // }
+  if (cpu.INTR) {
+    if(cpu.mstatus & MIE){ // 否则CPU处于关中断状态
+    cpu.INTR = false;
+    return IRQ_TIMER;
+    }
+  }
   return INTR_EMPTY;
 }
